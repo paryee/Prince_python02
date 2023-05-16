fe87e83319d5e962ea6b13e2cc59513f05fb804e
paryee <paryee2000@gmail.com>
2023-05-16 14:25:34 +0100

    this is my second project

---
+++ program2.py
@@ -0,0 +1,9 @@
+name = input("what is your name? ")
+age = input("what is your age? ")
+sex = input("enter your sex ")
+location = input("enter your location ")
+print(name)
+print(age)
+print(sex)
+print(location)
+
